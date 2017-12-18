83d49b8222139f6d57831d1afd09a744e0381798
BluntzerKevin <31923994+BluntzerKevin@users.noreply.github.com>
2017-12-18 01:41:40 +0100

    Ajout tp7

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -12,23 +13,29 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule }     from './app-routing.module';
 
 import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
+//import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+//import { InMemoryDataService }  from './in-memory-data.service';
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+ 
+
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    HeroSearchComponent
   ],
-  imports: [
-    BrowserModule,
-	FormsModule,
-	AppRoutingModule
-  ],
-  providers: [HeroService,MessageService ],
-  bootstrap: [AppComponent]
+  providers: [ HeroService, MessageService ],
+  bootstrap: [ AppComponent ]
 })
-
-export class AppModule { 
-
-}
+export class AppModule { }
